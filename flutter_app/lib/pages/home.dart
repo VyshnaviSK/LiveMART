6ee5fb4975d33eb0212bb1eca71fb9425da48087
apricot11hazel <apricot11hazel@gmail.com>
2021-04-05 11:34:43 +0530

    just trying it out

---
+++ flutter_app/lib/pages/home.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'sign-in.dart';
+import 'background.dart';
+import 'logged-in.dart';
+import 'sign-up.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final provider = Provider.of<GoogleSignInProvider>(context);
+
+          if (provider.isSigningIn) {
+            return buildLoading();
+          } else if (snapshot.hasData) {
+            return LoggedInWidget();
+          } else {
+            return SignUpWidget();
+          }
+        },
+      ),
+    ),
+  );
+
+  Widget buildLoading() => Stack(
+    fit: StackFit.expand,
+    children: [
+      CustomPaint(painter: BackgroundPainter()),
+      Center(child: CircularProgressIndicator()),
+    ],
+  );
+}
