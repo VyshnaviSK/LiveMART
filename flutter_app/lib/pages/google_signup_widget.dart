6305601481eb8f90c96307ea3fab33eebc436e80
kashishagrawal0912 <kashishagrawal0912@gmail.com>
2021-04-06 15:47:01 +0530

    Added changed deprecated button

--- flutter_app/lib/pages/google_signup_widget.dart
+++ flutter_app/lib/pages/google_signup_widget.dart
@@ -7,16 +7,16 @@ class GoogleSignupButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
     padding: EdgeInsets.all(4),
-    child: OutlineButton.icon(
+    child: OutlinedButton.icon(
       label: Text(
         'Sign In With Google',
         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
       ),
-      shape: StadiumBorder(),
+      /*shape: StadiumBorder(),
       padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
       highlightedBorderColor: Colors.black,
       borderSide: BorderSide(color: Colors.black),
-      textColor: Colors.black,
+      textColor: Colors.black,*/
       icon: FaIcon(FontAwesomeIcons.google, color: Colors.red),
       onPressed: () {
         final provider =
