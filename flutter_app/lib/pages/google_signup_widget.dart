fff7bda101321cc8f0e7e5fe59e8418045c17f0e
apricot11hazel <apricot11hazel@gmail.com>
2021-04-07 23:52:30 +0530

    new changes in firebase

--- flutter_app/lib/pages/google_signup_widget.dart
+++ flutter_app/lib/pages/google_signup_widget.dart
@@ -6,18 +6,58 @@ import 'package:provider/provider.dart';
 class GoogleSignupButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
-    padding: EdgeInsets.all(4),
-    child: OutlineButton.icon(
+    padding: const EdgeInsets.only(bottom: 16.0),
+    child: OutlinedButton.icon(
       label: Text(
         'Sign In With Google',
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20, color: Colors.black54,),
       ),
-      shape: StadiumBorder(),
+      style:ButtonStyle(
+  backgroundColor: MaterialStateProperty.all(Colors.white),
+  shape: MaterialStateProperty.all(
+  RoundedRectangleBorder(
+  borderRadius: BorderRadius.circular(40),
+  ),),
+      ),
+
+
+    /*child: Padding(
+    padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+    child: Row(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    Image(
+    image: AssetImage("assets/google_logo.png"),
+    height: 35.0,
+    ),
+    Padding(
+    padding: const EdgeInsets.only(left: 10),
+    child: Text(
+    'Sign in with Google',
+    style: TextStyle(
+    fontSize: 20,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    ),
+    ),
+    )
+    ],
+    ),
+    ),
+    ),
+    );
+    }
+    }
+*/
+
+
+      /*shape: StadiumBorder(),
       padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
       highlightedBorderColor: Colors.black,
       borderSide: BorderSide(color: Colors.black),
       textColor: Colors.black,
-      icon: FaIcon(FontAwesomeIcons.google, color: Colors.red),
+      icon: FaIcon(FontAwesomeIcons.google, color: Colors.red),*/
       onPressed: () {
         final provider =
         Provider.of<GoogleSignInProvider>(context, listen: false);
