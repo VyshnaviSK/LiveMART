d16baeb81b74c7e3eb97f87599c739eed5190545
apricot11hazel <61468965+apricot11hazel@users.noreply.github.com>
2021-04-24 18:13:30 +0530

    Update Shops.dart

--- flutter_app/lib/Models/Shops.dart
+++ flutter_app/lib/Models/Shops.dart
@@ -12,6 +12,8 @@
       "stock": "false",
       "availableDate": "20-07-2020"
     },
+    "category": {"fruits":
+    [
     {
       "name": "mango",
       "cost": 5,
@@ -35,7 +37,10 @@
       "cost": 10,
       "stock": "true",
       "availableDate": "12-08-2020"
-    },
+    }]},
+    {
+    "vegetables":
+    [
     {
       "name": "cauliflower",
       "cost": 25,
@@ -47,14 +52,35 @@
       "cost": 15,
       "stock": "false",
       "availableDate": "26-04-2021"
-    },
+    }]},
+    "cereals":
+    [
     {
       "name": "chickpeas",
       "cost": 40,
       "stock": "true",
       "availableDate": "22-06-2020"
     },
-  ];
+    {
+      "name": "pulses",
+      "cost": 55,
+      "stock": "true",
+      "availableDate": "19-02-2021"
+    },
+    {
+      "name": "rice",
+      "cost": 20,
+      "stock": "true",
+      "availableDate": "11-01-2020"
+    },
+    {
+      "name": "wheat",
+      "cost": 22,
+      "stock": "true",
+      "availableDate": "25-01-2020"
+    }]
+    }
+    ];
 
   var WholeShops = [
     {
@@ -69,6 +95,20 @@
       "stock": "false",
       "availableDate": "20-07-2020"
     },
+     {
+      "name": "saler1",
+      "cost": 26,
+      "distance": "4.5 km",
+      "Instock": "true"
+    },
+    {
+      "name": "saler2",
+      "cost": 19,
+      "stock": "false",
+      "availableDate": "20-07-2020"
+    },
+    "category": {"fruits":
+    [
     {
       "name": "mango",
       "cost": 5,
@@ -92,7 +132,10 @@
       "cost": 10,
       "stock": "true",
       "availableDate": "12-08-2020"
-    },
+    }]},
+    {
+    "vegetables":
+    [
     {
       "name": "cauliflower",
       "cost": 25,
@@ -104,11 +147,32 @@
       "cost": 15,
       "stock": "false",
       "availableDate": "26-04-2021"
-    },
+    }]},
+    "cereals":
+    [
     {
       "name": "chickpeas",
       "cost": 40,
       "stock": "true",
       "availableDate": "22-06-2020"
     },
-  ];
+    {
+      "name": "pulses",
+      "cost": 55,
+      "stock": "true",
+      "availableDate": "19-02-2021"
+    },
+    {
+      "name": "rice",
+      "cost": 20,
+      "stock": "true",
+      "availableDate": "11-01-2020"
+    },
+    {
+      "name": "wheat",
+      "cost": 22,
+      "stock": "true",
+      "availableDate": "25-01-2020"
+    }]
+    }
+    ];
